33ac1bb1b30ac7459d0fb6efcfc0d94d7851aff3
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-04-07 05:31:12 +0600

    about section completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import Navbar from "./components/navbar/Navbar";
 import Navbar2 from "./components/navbar/Navbar2";
+import About from "./sections/about/About";
+import Hero from "./sections/hero/Hero";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar2 />
+      <Hero />
+      <About />
     </div>
   );
 };
---
+++ client/src/sections/hero/Hero.js
@@ -0,0 +1,38 @@
+import React from "react";
+import HeroBg from "../../assets/images/hero-bg.png";
+import { Button } from "reactstrap";
+import { BsGithub } from "react-icons/bs";
+
+const Hero = () => {
+  return (
+    <div className="hero-container">
+      <div className="container container-ld hero-inner">
+        <div className="">
+          <div className="hero-info">
+            <p>
+              Intoducing <span className="text-primary">AKA</span>
+              <span className="text-yellow">COIN</span>
+            </p>
+            <h1>Living Ecosystem Decentralized Token</h1>
+            <span>
+              Akacoin is a open source peer-to-peer digital currency,community-run technology that supports
+              cryptocurrencies and thousands of decentralized applications.
+            </span>
+            <div className="hero-action">
+              <Button color="primary">Get Started</Button>
+              <Button className="button-white">
+                <BsGithub style={{ marginRight: "15px" }} />
+                Source
+              </Button>
+            </div>
+          </div>
+        </div>
+        <div className="hero-tree">
+          <img src={HeroBg} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
