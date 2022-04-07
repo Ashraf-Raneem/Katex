8e7f948f165c3a683924a77a54b07f7ed8fcb3d2
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-04-08 03:20:18 +0600

    advert section added

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/navbar/Navbar";
 import Navbar2 from "./components/navbar/Navbar2";
 import About from "./sections/about/About";
+import Advert from "./sections/advert/Advert";
 import Hero from "./sections/hero/Hero";
 import QuickStart from "./sections/quick-start/QuickStart";
 import WalletPage from "./sections/wallets/WalletPage";
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <WalletPage />
       <QuickStart />
+      <Advert />
     </div>
   );
 };
