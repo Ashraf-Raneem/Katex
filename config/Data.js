9b593c105a119d7381dfe5f62f90225d9ddcfffb
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-04-10 05:20:32 +0600

    backend initialized

---
+++ config/Data.js
@@ -0,0 +1,36 @@
+import HeadIcon1 from "../assets/images/icon-settings.png";
+import HeadIcon2 from "../assets/images/icon-safe.png";
+import HeadIcon3 from "../assets/images/icon-crown.png";
+
+const WalletData = [
+  {
+    id: 0,
+    theme: "primary",
+    head: {
+      icon: HeadIcon1,
+      text: "AkaWallet",
+    },
+    desc: "Start exploring blockchain applications in seconds. Trusted by over 1 million users worldwide",
+  },
+  {
+    id: 1,
+    theme: "secondary",
+    head: {
+      icon: HeadIcon2,
+      text: "AkaSafe",
+    },
+    desc: "AkaSafe wallet is a secure, decentralized, easy-to-use, and free application to manage more than 10,000 cryptocurrencies.",
+  },
+  {
+    id: 2,
+    theme: "primary",
+    head: {
+      icon: HeadIcon3,
+      text: "AkaMask",
+    },
+    desc: "AkaMask is a decentralized multi-chain digital wallet, dedicated to providing safe and convenient one-stop digital asset management services to users around the world.",
+  },
+];
+
+
+export default WalletData;
